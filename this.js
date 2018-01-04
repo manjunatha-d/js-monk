e9e7b5502ab26703ccc6a0bdb32fe9a1eccee47e
Manjunatha D <manjunathad13@gmail.com>
2018-01-04 22:21:08 +0530

    4 ways of determing `this`

--- this.js
+++ this.js
@@ -1,11 +1,19 @@
 /* 
     A program to show all the 4 ways in which `this` keyword is bound during runtime.
+
+    1. Default Binding (to global object in non-strict mode, to undefined in strict mode)
+    2. Implicit Binding (to the object that owns the function reference when it is invoked)
+    3. Explicit Binding (to the object passed via call(), apply(), or bind())
+    4. `new` Binding (to the newly constructed object)
+
+    `this` is a runtime thing. Its value depends on how the function is called. You need to examine the call site to figure out its value.
+    More info => https://github.com/getify/You-Dont-Know-JS/blob/master/this%20%26%20object%20prototypes/ch2.md
 */
 
 function foo() {
   var a = "`a` inside foo";
-  console.log("this === global:", this === global); // logs true
-  console.log("this.a`:", this.a); // logs undefined
+  console.log("this === global:", this === global); 
+  console.log("this.a`:", this.a); 
 }
 
 // var a = "global `a`";
@@ -38,3 +46,26 @@ obj1.foo();
 // logs
 // this === global: false
 // this.a`: obj1 `a`
+
+// Hard Binding
+
+/* 
+    function foo(something) {
+      console.log( this.a, something );
+      return this.a + something;
+    }
+
+    var obj = {
+      a: 2
+    };
+
+    // hard binding function. Passes through the arguments passed, and returns the value received
+    var bar = function() {
+      return foo.apply( obj, arguments );
+    };
+
+    var b = bar( 3 ); // 2 3
+    console.log( b ); // 5 
+*/
+
+// Since hard binding is a commonly used pattern, ES5 provided a built in utility => Function.prototype.bind
